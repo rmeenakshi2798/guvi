e149c82c0345dd71c38702971c150a4075b71124
Meenakshi <meenakshirswami39@gmail.com>
2019-01-27 09:55:46 +0530

    Update rm32.c

--- rm32.c
+++ rm32.c
@@ -3,14 +3,28 @@
 int main()
 {
     char a[100];
-    int i,c=1;
+    int i,c=0;
     scanf("%[^\n]",a);
     for(i=0;a[i]!='\0';i++)
     {
-        if(a[i]==' ')
+        if(a[0]!=' ')
+        {
         
+        if(a[i]==' ')
+        {
+            
+        if((a[i+1]!=' ')||(a[i+1]=='\0'))
+        {
             c++;
         
+    }
+        }
+    else if(a[i+1]=='\0')
+    {
+        c++;
+    }
+        
+        }
     }
     printf("%d",c);
 
