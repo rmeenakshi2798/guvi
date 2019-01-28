3d0b04cd1f08be328f12c537010323bd44b45e5f
Meenakshi <meenakshirswami39@gmail.com>
2019-01-28 19:38:35 +0530

    Update rm90.c

--- rm90.c
+++ rm90.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-   char a[10],i;
+   char a[100],i;
    scanf("%s",a);
    for(i=0;a[i]!='\0';i++)
    {
+       
        if(a[i]>='0'&&a[i]<='9')
        printf("%c",a[i]);
-   }
+       }
+  
     return 0;
 }
