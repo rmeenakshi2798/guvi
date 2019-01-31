bd0d390ff6e043f9df705749b3c828b35c5a3b70
Meenakshi <meenakshirswami39@gmail.com>
2019-01-31 06:45:57 +0530

    Create rm41.c

---
+++ rm41.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   char a[50];
+   int n,i;
+   scanf("%s",a);
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       printf("%s\n",a);
+   }
+    return 0;
+}
