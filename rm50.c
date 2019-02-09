77239155807a2486395b492c04dbde76ae6487b6
Meenakshi <meenakshirswami39@gmail.com>
2019-02-09 15:44:32 +0530

    Create rm50.c

---
+++ rm50.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+   int a,c,b;
+   scanf("%d",&a);
+   b=sqrt(a);
+   c=b*b;
+   if(c==a)
+   printf("yes");
+   else
+   printf("no");
+   
+   
+   
+
+    return 0;
+}
