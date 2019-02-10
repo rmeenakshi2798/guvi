7f221f6ca8eb3185065d54265f780cce9c045983
Meenakshi <meenakshirswami39@gmail.com>
2019-02-10 06:10:32 +0530

    Create rm53.c

---
+++ rm53.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,r,sum=0;
+   scanf("%d",&n);
+   while(n)
+   {
+       r=n%10;
+       sum=sum+r;
+       n=n/10;
+   }
+   printf("%d",sum);
+   
+    return 0;
+}
