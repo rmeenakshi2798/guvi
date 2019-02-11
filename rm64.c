f48ec02d139f0aa185dbcce94ec604172d46859a
Meenakshi <meenakshirswami39@gmail.com>
2019-02-11 15:40:26 +0530

    Create rm64.c

---
+++ rm64.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int c,m,n;
+    scanf("%d%d",&n,&m);
+    c=n+m;
+    if(c%2==0)
+    
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
