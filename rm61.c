09a1a1b032ceb7e08e13b3cc635243350b341ce4
Meenakshi <meenakshirswami39@gmail.com>
2019-02-11 15:28:46 +0530

    Create rm61.c

---
+++ rm61.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   char a[100];
+   int n,i;
+   scanf("%s",a);
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       printf("%c",a[i]);
+   }
+
+    return 0;
+}
