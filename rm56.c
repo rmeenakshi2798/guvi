6aa80757a32cdf6b2e538e762f179ce99ae353d2
Meenakshi <meenakshirswami39@gmail.com>
2019-02-16 16:33:29 +0530

    Update rm56.c

--- rm56.c
+++ rm56.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
 
-
 int main()
 {
-   char a[50];
-   int i,c1=0,c2=0,c3=0;
-  scanf("%[^\n]",a);
-   
-   for(i=0;a[i]!='\0';i++)
+   char a[100];
+   int c=0,p=0,i;
+   scanf("%s",a);
+   for(i=0;a[i]!=0;i++)
    {
-       if((a[i]>='a'&&a[i]<='z')||(a[i]>='Z'&&a[i]<='Z'))
+       if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
        {
-           c1++;
+           c=1;
        }
-       else if(a[i]>='0'&&a[i]<='9')
+       if(a[i]>='0'&&a[i]<='9')
        {
-           c2++;
-       }
-       else
-       {
-           c3++;
+           p=1;
        }
    }
-   if(c1!=0&&c2!=0&&c3==0)
-   {
-       printf("Yes");
-       }
+if(p==1&&c==1)
+{
+    printf("yes");
+}
 else
 {
-    printf("No");
+    printf("no");
 }
     return 0;
 }
